98893bbb0654a8237f387da006be5ce206ec5b14
richard.wobus@noaa.gov <richard.wobus@noaa.gov>
2014-10-17 16:05:40 +0000

    This line, and those below, will be ignored--     https://svnemc.ncep.noaa.gov/projects/gefs/branches/wcoss_production_initial/gfs_10.1.4_wcoss_production     delete directory created in the wrong place

--- gfs_10.1.4_wcoss_production/sorc/gefs_global_fcst.fd/deldifs_def.f
+++
@@ -1,7 +0,0 @@
-      module deldifs_def
-      use machine
-      implicit none
-      save
-      REAL(KIND=KIND_EVOD),ALLOCATABLE :: DNE(:),DNO(:),
-     . SF(:),RTRD(:),RTHK(:),BKLY(:),CKLY(:)			! hmhj
-      end module deldifs_def
