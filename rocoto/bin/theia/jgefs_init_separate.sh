bd5d8c777c93538eca25a4fd6bbcf6ee76974ee7
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2018-01-16 17:26:18 +0000

    Duplicate rocoto tasks and scripts from cray to theia
    Create new rocoto directories for theia tasks and scripts, and populate
    them with copies from cray as starting points.
    
    Change-Id: Ib16c804b722404a60b309641fa6d259649cd85e7
    Refs: #44962

---
+++ rocoto/bin/theia/jgefs_init_separate.sh
@@ -0,0 +1,66 @@
+#!/bin/ksh
+
+# EXPORT list here
+set -x
+export NODES=10
+export IOBUF_PARAMS=
+export FORT_BUFFERED=TRUE
+export MKL_CBWR=AVX
+ulimit -s unlimited
+ulimit -a
+
+export ATP_ENABLED=0
+export MALLOC_MMAP_MAX_=0
+export MALLOC_TRIM_THRESHOLD_=134217728
+
+export MPICH_ABORT_ON_ERROR=1
+export MPICH_ENV_DISPLAY=1
+export MPICH_VERSION_DISPLAY=1
+export MPICH_CPUMASK_DISPLAY=1
+
+export KMP_STACKSIZE=1024m
+export OMP_NUM_THREADS=6
+export KMP_AFFINITY=disabled
+
+#export OMP_NUM_THREADS=6
+export KMP_AFFINITY=disabled
+
+export MP_EUIDEVICE=sn_all
+export MP_EUILIB=us
+export MP_SHARED_MEMORY=no
+export MEMORY_AFFINITY=core:6
+
+export total_tasks=40
+export OMP_NUM_THREADS=6
+export taskspernode=4
+export MP_TASK_AFFINITY=cpu:6
+export MP_USE_BULK_XFER=yes
+export MP_STDOUTMODE=unordered
+export MPICH_ALLTOALL_THROTTLE=0
+export MP_COREFILE_FORMAT=core.txt
+export OMP_STACKSIZE=3G
+export MP_COMPILER=intel
+
+#Date and Cycle
+#export cyc=00
+#export PDY=20160415
+#export cyc_fcst=00
+#export job=Aa2016041500012
+
+# export for development runs only begin
+export envir=${envir:-dev}
+export RUN_ENVIR=${RUN_ENVIR:-dev}
+export gefsmachine=cray
+export gefsmpexec=" aprun -b -j1 -n40 -N4 -d6 -cc depth "
+export gefsmpexec_mpmd="  aprun -b -j1 -n40 -N4 -d6 -cc depth  cfp mpmd_cmdfile"
+export APRUNC="aprun"
+export aprun_gec00="aprun -b -j1 -n1 -N1 -d24 -cc depth"
+export NTHREADS_SIGCHGRS=6
+
+cd $SOURCEDIR/control
+. $SOURCEDIR/control/setbase
+#. $SOURCEDIR/parm/gefs.parm
+
+# CALL executable job script here
+$SOURCEDIR/jobs/JGEFS_INIT_SEPARATE
+
---
+++ rocoto/bin/theia/jgefs_post_track.sh
@@ -0,0 +1,61 @@
+#!/bin/ksh
+
+# EXPORT list here
+set -x
+export NODES=3
+export IOBUF_PARAMS=*:size=32M:count=4:verbose
+export FORT_BUFFERED=TRUE
+export MKL_CBWR=AVX
+ulimit -s unlimited
+ulimit -a
+
+export ATP_ENABLED=0
+export MALLOC_MMAP_MAX_=0
+export MALLOC_TRIM_THRESHOLD_=134217728
+
+export MPICH_ABORT_ON_ERROR=1
+export MPICH_ENV_DISPLAY=1
+export MPICH_VERSION_DISPLAY=1
+export MPICH_CPUMASK_DISPLAY=1
+
+export KMP_STACKSIZE=1024m
+export OMP_NUM_THREADS=1
+export KMP_AFFINITY=disabled
+
+#export OMP_NUM_THREADS=1
+export KMP_AFFINITY=disabled
+
+export MP_EUIDEVICE=sn_all
+export MP_EUILIB=us
+export MP_SHARED_MEMORY=no
+export MEMORY_AFFINITY=core:1
+
+export total_tasks=21
+export OMP_NUM_THREADS=1
+export taskspernode=7
+
+#Date and Cycle
+#export cyc=00
+#export cyc_fcst=00
+#export job=Aa2016041500396
+#export RUNMEM=gec00
+export FORECAST_SEGMENT=lr
+
+#export gefsmpexec_mpmd=mpirun.lsf
+
+# export for development runs only begin
+export envir=${envir:-dev}
+export RUN_ENVIR=${RUN_ENVIR:-dev}
+export gefsmachine=cray
+export gefsmpexec=" aprun -b -j1 -n21 -N7 -d1 -cc depth "
+export gefsmpexec_mpmd="  aprun -b -j1 -n21 -N7 -d1 -cc depth  cfp mpmd_cmdfile"
+export APRUNC="aprun"
+export aprun_gec00="aprun -b -j1 -n1 -N1 -d24 -cc depth"
+export NTHREADS_SIGCHGRS=6
+
+cd $SOURCEDIR/control
+. $SOURCEDIR/control/setbase
+#. $SOURCEDIR/parm/gefs.parm
+
+# CALL executable job script here
+$SOURCEDIR/jobs/JGEFS_POST_TRACK
