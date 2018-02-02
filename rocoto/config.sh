0f04c609a546058969fe9687f058537a4ef27275
Bing Fu <bing.fu@noaa.gov>
2018-02-02 14:01:59 +0000

    ISSUE #44960 updated GEFS following the updated fv3gfs,FV3,NEMS and post.
    Change-Id: I3267527bd3a230575eb857092c67df4c0da4ca97

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -1,8 +1,8 @@
-sed -e "s/sYYYYMMDDHH/2017121312/"               \
-    -e "s/eYYYYMMDDHH/2017121312/"               \
-    -e "s/TEST/tgBd/"                            \
-    -e "s/HPS/hps/"                              \
-    -e "s/First/Dingchen/"                       \
-    -e "s/Last/Hou/"                             \
+sed -e "s/sYYYYMMDDHH/2018013100/"               \
+    -e "s/eYYYYMMDDHH/2018013100/"               \
+    -e "s/TEST/BF44960/"                            \
+    -e "s/HPS/hps3/"                              \
+    -e "s/First/Bing/"                       \
+    -e "s/Last/Fu/"                             \
     user.conf_protype                            \
     > user.conf
