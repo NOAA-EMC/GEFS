005af969d3dc59f2b6cb72e40140e8a511e63623
Walter.Kolczynski <Walter.Kolczynski@noaa.gov>
2018-03-11 02:54:32 +0000

    Fix bugs with user.conf
    Replaced double quotes with single quotes in user.conf_prototype to
    prevent issues with existing double quotes in gefs.xml.in.
    
    Changed dummy variable HPS with HPS_PTMP to prevent the partial
    replacement of HPSS_DIR by config.sh.
    
    Change-Id: Ic3ef4c125f984d51f6c77fa5a741ca14941893da
    Refs: #46726

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -1,7 +1,7 @@
 sed -e "s/sYYYYMMDDHH/2018013100/"               \
     -e "s/eYYYYMMDDHH/2018013100/"               \
     -e "s/TEST/XXXX/"                            \
-    -e "s/HPS/hps/"                              \
+    -e "s/HPS_PTMP/hps/"                              \
     -e "s/First/XXXX/"                       \
     -e "s/Last/XXXX/"                             \
     user.conf_protype                            \
