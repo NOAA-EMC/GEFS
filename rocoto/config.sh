f4322e9d77fa63dc4e72e0a9298fbb520076c964
xiaqiong.zhou <xiaqiong.zhou@noaa.gov>
2018-04-06 12:26:15 +0000

    Add the job to read and recenter FV3 Q1FY19 EnKF nemsio for the next job to change resolution to 6 tile files for fcst job
    Change-Id: Ie1685c8a935cdd05f61f39e26158df11a1b63f36

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -1,8 +1,8 @@
 sed -e "s/sYYYYMMDDHH/2018013100/"               \
     -e "s/eYYYYMMDDHH/2018013100/"               \
-    -e "s/TEST/XXXX/"                            \
+    -e "s/TEST/xzinit/"                            \
     -e "s/HPS_PTMP/hps/"                              \
-    -e "s/First/XXXX/"                       \
-    -e "s/Last/XXXX/"                             \
+    -e "s/First/Xiaqiong/"                       \
+    -e "s/Last/Zhou/"                             \
     user.conf_protype                            \
     > user.conf
