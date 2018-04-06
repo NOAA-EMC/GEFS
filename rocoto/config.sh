6a46dcbce8935cd05a0708bcc58c007beeb7d450
dingchen.hou <dingchen.hou@noaa.gov>
2018-04-06 17:27:43 +0000

    Revert "ISSUE 44963 minor update to rocoto/config.sh"
    This reverts commit 9deda900d706e2b00cae5368f602697f5adfedaa.

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -4,7 +4,7 @@ sed -e "s/sYYYYMMDDHH/2018013100/"            \
     -e "s/HPS_PTMP/hps/"                      \
     -e "s/First/XXXX/"                        \
     -e "s/Last/XXXX/"                         \
-    -e "s/sACCOUNT/GEN-T20/"                  \ GEN will be avaiable 
-    -e "s/sCUE2RUN/dev/"                      \ dev for GEN and devhigh for GEN-T2O
+    -e "s/sACCOUNT/GEN-T20/"                     \
+    -e "s/sCUE2RUN/devhigh/"                     \
     user.conf_protype                         \
     > user.conf
