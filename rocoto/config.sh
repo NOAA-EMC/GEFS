a1521a346185809b5ac4917f6aa0c2ab412ab961
Xianwu Xue <Xianwu.Xue@noaa.gov>
2018-04-06 15:57:07 +0000

    add ACCOUNT and CUE2RUN options in rocoto

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -1,8 +1,10 @@
-sed -e "s/sYYYYMMDDHH/2018013100/"               \
-    -e "s/eYYYYMMDDHH/2018013100/"               \
-    -e "s/TEST/XXXX/"                            \
-    -e "s/HPS_PTMP/hps/"                              \
-    -e "s/First/XXXX/"                       \
-    -e "s/Last/XXXX/"                             \
-    user.conf_protype                            \
+sed -e "s/sYYYYMMDDHH/2018013100/"            \
+    -e "s/eYYYYMMDDHH/2018013100/"            \
+    -e "s/TEST/XXXX/"                         \
+    -e "s/HPS_PTMP/hps/"                      \
+    -e "s/First/XXXX/"                        \
+    -e "s/Last/XXXX/"                         \
+    -e "s/sACCOUNT/GEN-T20/"                     \
+    -e "s/sCUE2RUN/devhigh/"                     \
+    user.conf_protype                         \
     > user.conf
