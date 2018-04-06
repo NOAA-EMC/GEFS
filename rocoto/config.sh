e385a7a457d5bea758a6ec40c045e2cb4706f454
dingchen.hou <dingchen.hou@noaa.gov>
2018-04-06 17:34:21 +0000

    ISSUE #44963 Minor change to rocoto/config.sh
    Change-Id: I18b7742416733c28a8b3f8b8cf0e47ddf21c6f8f

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -4,7 +4,9 @@ sed -e "s/sYYYYMMDDHH/2018013100/"            \
     -e "s/HPS_PTMP/hps/"                      \
     -e "s/First/XXXX/"                        \
     -e "s/Last/XXXX/"                         \
-    -e "s/sACCOUNT/GEN-T20/"                     \
-    -e "s/sCUE2RUN/devhigh/"                     \
+    -e "s/sACCOUNT/GEN-T20/"                  \
+    -e "s/sCUE2RUN/devhigh/"                  \
     user.conf_protype                         \
     > user.conf
+### Note for WCOSS: For sACCOUNT, GEN is suggested (if available) except devhigh users
+### Note for WCOSS: For sCUE2RUN, dev for GEN and devhigh for GEN-T2O is recommended 
