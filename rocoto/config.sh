b815c21db0f001211e12230d96d43d2c7af26c42
Dingchen <dingchen.hou@noaa.gov>
2018-04-18 13:24:46 +0000

    ISSUE #49189 update J-job and rocoto (cray only) files to unify the specification of output frequency     1) A bug fixed in the master for rocoto/config.sh (GEN-T2O)     2) Added FHOUTHR in rocoto/parm/gefs_dev.parm     3) Modified J-job files for necppost, prdgen, ensstat to get rid of hardwired output frequency     4) Use 3 (vs 5) tasks for prodgen_high  (in rocoto/tasks/cray and bin/tasks/cray)     5) Increase walltime for keep_date to 1:00.
    Change-Id: I383f8af52ced2e9cf6aa7031fe9afd9492263f70

--- rocoto/config.sh
+++ rocoto/config.sh
@@ -4,7 +4,7 @@ sed -e "s/sYYYYMMDDHH/2018013100/"            \
     -e "s/HPS_PTMP/hps/"                      \
     -e "s/First/XXXX/"                        \
     -e "s/Last/XXXX/"                         \
-    -e "s/sACCOUNT/GEN-T20/"                  \
+    -e "s/sACCOUNT/GEN-T2O/"                  \
     -e "s/sCUE2RUN/devhigh/"                  \
     user.conf_protype                         \
     > user.conf
