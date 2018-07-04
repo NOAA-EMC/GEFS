874bd66bc6b96f4573ca65ef155fdc3e51736a2f
Xianwu Xue <Xianwu.Xue@noaa.gov>
2018-07-04 12:48:24 -0400

    ISSUE #52246 Make changes to run updated FV3 based GEFS on Theia 1) Add jgefs_init_recenter.sh for Theia 2) Modify setbase and gefs_config files   a) Make consistent with the configuration for theia and cray   b) Make the libs to point to the common libs on theia 3) Modify user configure files for Theia 4) Fix a small bug for task recenter on CRAY, Theia and WCOSS 5) Added user_full_ForTest.conf for the test on Theia using Bing Fu's initial data 6) Try to create a new bash script to unify compilling, installing and clear GEFS, as well as running rocoto
     Changes to be committed:
    	new file:   bin/theia/jgefs_init_recenter.sh
    	modified:   bin/theia/jgefs_post_high.sh
    	new file:   compile_install_all.sh
    	modified:   parm/gefs_config
    	modified:   parm/setbase
    	modified:   py/user_cray.conf
    	modified:   py/user_theia.conf
    	modified:   py/user_wcoss.conf
    	new file:   user_full_ForTest.conf

---
+++ rocoto_new/compile_install_all.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+
+sWS=`pwd`
+echo $sWS
+
+while getopts c:a:r:m:f: option
+do
+	case "${option}"
+	in
+		c) CompileCode=${OPTARG};;
+		a) CleanAll=${OPTARG};;
+		r) RunRocoto=${OPTARG};;
+ 		m) machine=${OPTARG};;
+		f) userConfigFile=${OPTARG};;
+	esac
+done
+
+CompileCode=${CompileCode:-no}
+CleanAll=${CleanAll:-no}
+RunRocoto=${RunRocoto:-no}
+machine=${machine:-none}
+userConfigFile=${userConfigFile:-user_full.conf}
+
+
+if [ $machine = none ]; then
+	if [ -d /scratch4/NCEPDEV ]; then
+		machine=theia
+	elif [ -d /gpfs ]; then
+		if [ -d /etc/SuSE-release ]; then
+		  machine=cray
+		fi
+	fi
+fi
+
+echo $CompileCode
+echo $CleanAll
+echo $RunRocoto
+echo $machine
+echo $userConfigFile
+
+
+if [ $CompileCode = yes ]; then
+	cd $sWS/../sorc
+
+	## mkdir folds
+	mkdir ../exec
+	mkdir ../util/exec
+
+	## Deal with the modules
+	module purge
+	module use ./
+	
+	if [ $machine = theia ]; then
+		echo "You are running on Theia!"
+		module load Module_gefs_v12_theia
+	elif [ $machine = cray ]; then
+		echo "You are running on Cray!"
+		module load Module_gefs_v12_cray
+	elif [ $machine = wcoss ]; then
+		echo "You are running on wcoss!"
+		module load Module_gefs_v12_wcoss
+	else
+		echo "You are running on some platform we didn't support, please check it!"
+		exit
+	fi
+
+	## Build the code
+	./build.sh
+
+	## Install GEFS
+	./install.sh
+fi
+
+# for cleanning
+if [ $CleanAll = yes ]; then
+  echo "Cleaning ..."
+
+ 	cd $sWS/../sorc
+
+	for dir in gefs_vortex_separate.fd gefs_vortex_combine.fd global_sigzvd.fd  global_ensadd.fd  global_enspqpf.fd  gefs_ensstat.fd  global_ensppf.fd ; do
+		  cd $dir
+		  make clean
+		  cd ..
+	done
+
+	export LIBS="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
+
+	for dir in global_enscvprcp.fd  global_enspvrfy.fd  global_enssrbias.fd global_enscqpf.fd  global_enscvt24h.fd  global_ensrfmat.fd ; do
+		  cd $dir
+		  make clean
+		  cd ..
+	done
+
+	for dir in ../util/sorc/gettrk.fd ../util/sorc/overenstr.grib.fd ../util/sorc/getnsttf.fd; do
+		  cd $dir
+		  make clean
+		  cd ../../../sorc
+	done
+
+	cd $sWS/../sorc
+  rm -rf ../exec
+  rm -rf ../util/exec
+
+
+fi
+
+# for rocoto
+
+if [ $RunRocoto = yes ]; then
+	cd $sWS
+	module load rocoto
+	#module load python
+	./py/run_to_get_all.py  $userConfigFile
+fi
+
+
