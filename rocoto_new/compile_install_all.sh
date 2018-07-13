4d86127012b2b982b721a7094ce2fe2a9c828907
Xianwu Xue <Xianwu.Xue@noaa.gov>
2018-07-13 17:36:31 -0400

    Replace the TAB in the compile_install_all.sh

--- rocoto_new/compile_install_all.sh
+++ rocoto_new/compile_install_all.sh
@@ -5,14 +5,14 @@ echo $sWS
 
 while getopts c:a:r:m:f: option
 do
-	case "${option}"
-	in
-		c) CompileCode=${OPTARG};;
-		a) CleanAll=${OPTARG};;
-		r) RunRocoto=${OPTARG};;
- 		m) machine=${OPTARG};;
-		f) userConfigFile=${OPTARG};;
-	esac
+    case "${option}"
+    in
+        c) CompileCode=${OPTARG};;
+        a) CleanAll=${OPTARG};;
+        r) RunRocoto=${OPTARG};;
+        m) machine=${OPTARG};;
+        f) userConfigFile=${OPTARG};;
+    esac
 done
 
 CompileCode=${CompileCode:-no}
@@ -23,13 +23,13 @@ userConfigFile=${userConfigFile:-user_full.conf}
 
 
 if [ $machine = "nomachine" ]; then
-	if [ -d /scratch4/NCEPDEV ]; then
-		machine=theia
-	elif [ -d /gpfs ]; then
-		if [ -d /etc/SuSE-release ]; then
-		  machine=cray
-		fi
-	fi
+    if [ -d /scratch4/NCEPDEV ]; then
+        machine=theia
+    elif [ -d /gpfs ]; then
+        if [ -d /etc/SuSE-release ]; then
+          machine=cray
+        fi
+    fi
 fi
 
 echo $CompileCode
@@ -40,64 +40,64 @@ echo $userConfigFile
 
 
 if [ $CompileCode = "yes" ]; then
-	cd $sWS/../sorc
+    cd $sWS/../sorc
 
-	## mkdir folds
-	mkdir ../exec
-	mkdir ../util/exec
+    ## mkdir folds
+    mkdir ../exec
+    mkdir ../util/exec
 
-	## Deal with the modules
-	module purge
-	module use ./
-	
-	if [ $machine = theia ]; then
-		echo "You are running on Theia!"
-		module load Module_gefs_v12_theia
-	elif [ $machine = cray ]; then
-		echo "You are running on Cray!"
-		module load Module_gefs_v12_cray
-	elif [ $machine = wcoss ]; then
-		echo "You are running on wcoss!"
-		module load Module_gefs_v12_wcoss
-	else
-		echo "You are running on some platform we didn't support, please check it!"
-		exit
-	fi
+    ## Deal with the modules
+    module purge
+    module use ./
 
-	## Build the code
-	./build.sh
+    if [ $machine = theia ]; then
+        echo "You are running on Theia!"
+        module load Module_gefs_v12_theia
+    elif [ $machine = cray ]; then
+        echo "You are running on Cray!"
+        module load Module_gefs_v12_cray
+    elif [ $machine = wcoss ]; then
+        echo "You are running on wcoss!"
+        module load Module_gefs_v12_wcoss
+    else
+        echo "You are running on some platform we didn't support, please check it!"
+        exit
+    fi
 
-	## Install GEFS
-	./install.sh
+    ## Build the code
+    ./build.sh
+
+    ## Install GEFS
+    ./install.sh
 fi
 
 # for cleanning
 if [ $CleanAll = "yes" ]; then
   echo "Cleaning ..."
 
- 	cd $sWS/../sorc
+    cd $sWS/../sorc
 
-	for dir in gefs_vortex_separate.fd gefs_vortex_combine.fd global_sigzvd.fd  global_ensadd.fd  global_enspqpf.fd  gefs_ensstat.fd  global_ensppf.fd ; do
-		  cd $dir
-		  make clean
-		  cd ..
-	done
+    for dir in gefs_vortex_separate.fd gefs_vortex_combine.fd global_sigzvd.fd  global_ensadd.fd  global_enspqpf.fd  gefs_ensstat.fd  global_ensppf.fd ; do
+          cd $dir
+          make clean
+          cd ..
+    done
 
-	export LIBS="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
+    export LIBS="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
 
-	for dir in global_enscvprcp.fd  global_enspvrfy.fd  global_enssrbias.fd global_enscqpf.fd  global_enscvt24h.fd  global_ensrfmat.fd ; do
-		  cd $dir
-		  make clean
-		  cd ..
-	done
+    for dir in global_enscvprcp.fd  global_enspvrfy.fd  global_enssrbias.fd global_enscqpf.fd  global_enscvt24h.fd  global_ensrfmat.fd ; do
+          cd $dir
+          make clean
+          cd ..
+    done
 
-	for dir in ../util/sorc/gettrk.fd ../util/sorc/overenstr.grib.fd ../util/sorc/getnsttf.fd; do
-		  cd $dir
-		  make clean
-		  cd ../../../sorc
-	done
+    for dir in ../util/sorc/gettrk.fd ../util/sorc/overenstr.grib.fd ../util/sorc/getnsttf.fd; do
+          cd $dir
+          make clean
+          cd ../../../sorc
+    done
 
-	cd $sWS/../sorc
+    cd $sWS/../sorc
   rm -rf ../exec
   rm -rf ../util/exec
 
@@ -107,10 +107,10 @@ fi
 # for rocoto
 
 if [ $RunRocoto = "yes" ]; then
-	cd $sWS
-	module load rocoto
-	#module load python
-	./py/run_to_get_all.py  $userConfigFile
+    cd $sWS
+    module load rocoto
+    #module load python
+    ./py/run_to_get_all.py  $userConfigFile
 fi
 
 
