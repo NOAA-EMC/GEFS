995084c51f6b7877de76868fd514e5d7dc3af7c3
Xianwu Xue <Xianwu.Xue@noaa.gov>
2019-01-31 15:43:50 -0500

    Ported to dell3 for the fortran codes
     On branch GEFS_production_2015
     Changes to be committed:
    	new file:   Module_gefs_v11.2.0_dell3
    	modified:   build.sh
    	modified:   build_all.sh
    	modified:   gefs_vortex_combine.fd/makefile
    	modified:   gefs_vortex_separate.fd/makefile
    	modified:   global_sigzvd.fd/makefile
    	modified:   ../util/sorc/getnsttf.fd/Makefile.conf
    
    Refs: #44907

--- sorc/build.sh
+++ sorc/build.sh
@@ -21,6 +21,7 @@ if [ $mac = t -o $mac = e -o $mac = g ] ; then # For WCOSS
 
  export FFLAGS="-O3 -g -convert big_endian -I ${G2_INC4}"
  export FFLAGS_d="-O3 -g -r8 -convert big_endian -auto -mkl -I ${G2_INCd}"
+ export OPENMPFFLAG="openmp"
 
 #---------------------------------------------------------
 
@@ -39,8 +40,36 @@ elif [ $mac = z -o $mac = h -o $mac = f ] ; then # For ZEUS
  export LIT_GTRK="-L/contrib/nceplibs/nwprod/lib -lip_4 -lsp_4 -lbacio_4 -lsigio_4 -lsfcio_4 -lbufr_4_64 -lw3emc_4 -lw3nco_4"
  export FC=ifort 
  export FFLAGS="-openmp -O3 -g -traceback -r8 -I "
+ export OPENMPFFLAG="openmp"
+else
+
+  if [[ -L /usrx && "$( readlink /usrx 2> /dev/null )" =~ dell ]] ; then # We are on NOAA Mars or Venus
+    echo "Building for wcoss_dell_p3"
+
+    machine=wcoss_dell_p3
+
+    export IP_LIBd=/gpfs/dell1/nco/ops/nwtest/lib.p2/ip/v2.0.0/libip_v2.0.0_d.a
+    export IP_LIB4=/gpfs/dell1/nco/ops/nwtest/lib.p2/ip/v2.0.0/libip_v2.0.0_4.a
+    export LIBDIR=/gpfs/dell1/nco/ops/nwprod/lib
+    export INCG="$NEMSIO_INC"
+    export INCGFS="$NEMSIOGFS_INC"
+    export INCS="${SIGIO_INC4}"
+    export INCSFC="${SFCIO_INC4}"
+    export INC="${G2_INC4}"
+    export LIBS="${G2_LIB4} ${W3NCO_LIB4} ${BACIO_LIB4} ${JASPER_LIB} ${PNG_LIB} ${Z_LIB}"
+    export INC_d="${G2_INCd}"
+    export LIBS_d="${G2_LIBd} ${W3NCO_LIBd} ${BACIO_LIB4} ${IP_LIBd} ${SP_LIBd} ${PNG_LIB} ${JASPER_LIB} ${Z_LIB} ${W3NCO_LIBd}"
+    export FC=ifort
+    export LIBS_INIT="${NEMSIOGFS_LIB} ${NEMSIO_LIB} ${SP_LIBd} ${SIGIO_LIB4} ${W3NCO_LIBd} ${BACIO_LIB4}"
+    export LIBS_GTRK="${NEMSIOGFS_LIB} ${NEMSIO_LIB} ${BACIO_LIB4} ${SIGIO_LIB4} ${IP_LIB4} ${SP_LIB4} ${SFCIO_LIB4} ${BUFR_LIB4} ${W3EMC_LIB4} ${W3NCO_LIB4} "
+    export FFLAGS="-O3 -g -convert big_endian -I ${G2_INC4}"
+    export FFLAGS_d="-O3 -g -r8 -convert big_endian -auto -mkl -I ${G2_INCd}"
+    export OPENMPFFLAG=qopenmp
+  fi
+
 
 fi
+
 #---------------------------------------------------------
 for dir in gefs_vortex_separate.fd gefs_vortex_combine.fd global_sigzvd.fd  global_ensadd.fd  global_enspqpf.fd  gefs_ensstat.fd  global_ensppf.fd global_enscvprcp.fd  global_enspvrfy.fd  global_enssrbias.fd global_enscqpf.fd  global_enscvt24h.fd  global_ensrfmat.fd ; do
 #for dir in global_enspvrfy.fd ;do
@@ -55,3 +84,13 @@ for dir in ../util/sorc/gettrk.fd ../util/sorc/overenstr.grib.fd ../util/sorc/ge
  make -f Makefile
  cd ../../../sorc
 done
+if [[ -L /usrx && "$( readlink /usrx 2> /dev/null )" =~ dell ]] ; then # We are on NOAA Mars or Venus
+    for dir in ../util/sorc/nemsio_get.fd; do
+        export FCMP=ifort
+        cd $dir
+        #make clean
+        make -f makefile
+        cd ../../../sorc
+    done
+fi
+
--- sorc/build_all.sh
+++ sorc/build_all.sh
@@ -9,15 +9,32 @@ target=${1:-wcoss}    ;#wcoss
 
 pwd=`pwd`
 
+if [ ! -d "../exec" ]; then 
+  echo "Creating ../exec folder"
+  mkdir ../exec
+fi
+if [ ! -d "../util/exec" ]; then
+  echo "Creating ../util/exec folder"
+  mkdir ../util/exec
+fi
+
 cd $pwd
 module purge
 module use ./
-module load Module_gefs_v11.2.0
+if [[ -L /usrx && "$( readlink /usrx 2> /dev/null )" =~ dell ]] ; then # We are on NOAA Mars or Venus
+    module load Module_gefs_v11.2.0_dell3
+else
+    module load Module_gefs_v11.2.0
+fi
+
 sh build.sh
 sh install.sh
 
-cd ../util/sorc
-module purge
-module use ./
-./build_nems_util.sh
+if [[ -d /dcom && -d /hwrf ]] ; then # Tide or Gyre
+    cd ../util/sorc
+    module purge
+    module use ./
+    ./build_nems_util.sh
+fi
+
 cd $pwd
