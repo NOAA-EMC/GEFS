6c47c4d7a91b6bd86c0b787f89b7524231f9fab0
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2019-06-06 09:53:55 -0400

    Add postsnd and avg_nemsio to default user configure files on cray and dell Fix a cpu setting for postsnd on dell
     On branch DHpostsnd
     Changes to be committed:
    	modified:   bin/wcoss_dell_p3/postsnd.sh
    	modified:   py/user_cray.conf
    	modified:   py/user_wcoss_dell_p3.conf
    	modified:   user_full.conf
    
    Refs: #64012, #64583 and #63797

--- rocoto/bin/wcoss_dell_p3/postsnd.sh
+++ rocoto/bin/wcoss_dell_p3/postsnd.sh
@@ -28,7 +28,7 @@ export MP_EUILIB=us
 export MP_SHARED_MEMORY=yes
 export MEMORY_AFFINITY=core:2
 
-export NODES=2
+export NODES=1
 export total_tasks=5
 export OMP_NUM_THREADS=2
 export taskspernode=12
