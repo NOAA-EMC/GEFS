5715897047d1d2e4fb3049811fa884872224f253
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2019-09-24 18:52:08 +0000

    Revamp python task scripts
    Python task scripts are updated with some of the Python 3 features that had
    formerly only been in the Theia versions (like using partial to force print
    to flush immediately). Also a lot of changes to make the code clearer and
    conform to Python style guidelines, including:
    
    - Most string concatenation replaced in favor of string format with named
        placeholders.
    - Changing the script description at the top from a series of comment lines
        to a docstring.
    - Replace '== None' with 'is None' in if statements.
    - Separate imports to one-per-line (and removed unused imports).
    
    Change-Id: I4869fff2355668885527c231e14857adb6ea8885
    Refs: #58418

--- rocoto/bin/py/keep_init.py
+++ rocoto/bin/py/keep_init.py
@@ -1,87 +1,85 @@
-#!/usr/bin/env python
-###! /usrx/local/prod/python/2.7.13/bin/python
+#! /usr/bin/env python3
 
-##########################################################
-# Copies specified directories of GEFS output from temporary location to another location.
-#
-# Inputs (via environment variables):
-# 	WORKDIR         : The base GEFS output directory (usually in ptmp)
-# 	INIT_DIR        : The base directory to store GEFS init files (usually in noscrub)
-# 	PDY             : Initialization date in YYYYMMDD form
-# 	cyc             : Initialization hour in HH form
-#
-# 	GEFS init files must be located in the WORKDIR com directory as such:
-# 		<WORKDIR>/nwges/dev/gefs.<PDY>/<cyc>/<MEMBER>
-#
-# Outputs:
-# 	For each directory in DIRS_TO_KEEP, that directory will be copied to the INIT_DIR in
-#	  the following location:
-#		<INIT_DIR>/gefs.<PDY>/<cyc>/<MEMBER>
-#
-# Error Codes:
-# 	-100 : Required environment variable not defined
-# 	-102 : INIT_DIR already exists and couldn't be deleted
-#
-##########################################################
+"""
+Copies specified directories of GEFS output from temporary location to another location.
 
-import os, shutil, glob, sys, errno
+Inputs (via environment variables):
+	WORKDIR         : The base GEFS output directory (usually in ptmp)
+	INIT_DIR        : The base directory to store GEFS init files (usually in noscrub)
+	PDY             : Initialization date in YYYYMMDD form
+	cyc             : Initialization hour in HH form
+
+	GEFS init files must be located in the WORKDIR com directory as such:
+		<WORKDIR>/nwges/dev/gefs.<PDY>/<cyc>/<MEMBER>
+
+Outputs:
+	For each directory in DIRS_TO_KEEP, that directory will be copied to the INIT_DIR in
+	the following location:
+		<INIT_DIR>/gefs.<PDY>/<cyc>/<MEMBER>
+
+Error Codes:
+	-100 : Required environment variable not defined
+	-102 : INIT_DIR already exists and couldn't be deleted
+
+"""
+import os
+import shutil
+from functools import partial
+
+destination_base = "{init_dir}/{pdy}/{cycle}/{member}"
+destination_pattern = "{init_dir}/{pdy}/{cycle}/{member}"
+source_pattern = "{work_dir}/nwges/dev/gefs.{pdy}/{cycle}/{member}"
+clobber = True
+
+# Make sure print statements are flushed immediately, otherwise
+#   print statments may be out-of-order with subprocess output
+print = partial(print, flush=True)
 
 # Read in environment variables and make sure they exist
 work_dir = os.environ.get("WORKDIR")
-if( work_dir == None ):
+if( work_dir is None ):
 	print("FATAL: Environment variable WORKDIR not set")
 	quit(-100)
 
 init_dir = os.environ.get("INIT_DIR")
-if( init_dir == None ):
+if( init_dir is None ):
 	print("FATAL: Environment variable INIT_DIR not set")
 	quit(-100)
 
-date_string = os.environ.get("PDY")
-if( date_string == None ):
+pdy = os.environ.get("PDY")
+if( pdy is None ):
 	print("FATAL: Environment variable PDY not set")
 	quit(-100)
 
 cycle = os.environ.get("cyc")
-if( cycle == None ):
+if( cycle is None ):
 	print("FATAL: Environment variable cyc not set")
 	quit(-100)
 
 member = os.environ.get("MEMBER")
-if( member == None ):
+if( member is None ):
 	print("FATAL: Environment variable MEMBER not set")
 	quit(-100)
 
-clobber = True
 
 print("Starting GEFS keep_init with the following settings:")
-print("Source Directory      : " + work_dir)
-print("Destination Directory : " + init_dir)
-print("Member                : " + member)
-print("Date/Cycle            : " + date_string + "_" + cycle)
-sys.stdout.flush()
+print("Source Directory      : {work_dir}".format(work_dir=work_dir))
+print("Destination Directory : {init_dir}".format(init_dir=init_dir))
+print("Member                : {member}".format(member=member))
+print("Date/Cycle            : {pdy}_{cycle}".format(pdy=pdy, cycle=cycle))
 
-source_dir = work_dir + "/nwges/dev/gefs." + date_string + "/" + cycle + "/" + member
-destination_base = init_dir + "/" + date_string + "/" + cycle
-destination_dir = destination_base + "/" + member
+source_dir = source_pattern.format( work_dir=work_dir, pdy=pdy, cycle=cycle, member=member )
+destination_dir = destination_pattern.format( init_dir=init_dir, pdy=pdy, cycle=cycle, member=member )
+
+# os.makedirs(destination_base, exist_ok=True)
+
 if ( os.path.exists(destination_dir) ):
 	if(clobber):
 		shutil.rmtree(destination_dir)
 	else:
 		print(destination_dir + " already exists and clobber is False, skipping.")
-		sys.stdout.flush()
 		quit(0)
-
-if ( not os.path.exists(destination_base) ):
-	try:
-		os.makedirs(destination_base)
-	except OSError as e:
-		if e.errno == errno.EEXIST:
-			pass
-		else:
-			raise
 
 print("Copying files for init")
 print("    From " + source_dir + " to " + destination_dir)
-sys.stdout.flush()
 shutil.copytree(source_dir, destination_dir, symlinks=True)
