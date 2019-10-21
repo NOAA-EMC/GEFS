a6ac4b142568c16dc68afc74074c48ecc623b2ea
Bing Fu <bing.fu@noaa.gov>
2019-10-21 14:59:12 +0000

    break archive job into sub jobs for 0.25 grib data on Dell
    Change-Id: I490396f829f52aafa63732f286d267b9465923d3

---
+++ rocoto/bin/wcoss_dell_p3/archivep25a.sh
@@ -0,0 +1,54 @@
+#!/bin/ksh
+#
+
+# EXPORT list here
+set -x
+
+ulimit -s unlimited
+ulimit -a
+#export MPICH_ABORT_ON_ERROR=1
+#export MPICH_ENV_DISPLAY=1
+#export MPICH_VERSION_DISPLAY=1
+#export MPICH_CPUMASK_DISPLAY=1
+
+#export KMP_STACKSIZE=1024m
+#export KMP_AFFINITY=disabled
+
+#export MP_EUIDEVICE=sn_all
+#export MP_EUILIB=us
+#export MP_SHARED_MEMORY=yes
+#export MEMORY_AFFINITY=core:2
+
+#export NODES=1
+#export total_tasks=48
+#export OMP_NUM_THREADS=2
+#export taskspernode=12
+
+
+# export for development runs only begin
+export envir=${envir:-dev}
+export RUN_ENVIR=${RUN_ENVIR:-dev}
+
+. $GEFS_ROCOTO/parm/setbase
+. $GEFS_ROCOTO/parm/gefs_config
+. $GEFS_ROCOTO/parm/gefs_dev.parm
+
+# CALL executable job script here
+HPSS_DIR=$HPSS_DIR
+WORKDIR=$WORKDIR
+PDY=$PDY
+cyc=$cyc
+yyyy=${PDY:0:4}
+mm=${PDY:4:2}
+dd=${PDY:6:2}
+
+destination_path=${HPSS_DIR}/${yyyy}/${yyyy}${mm}/${yyyy}${mm}${dd}
+output_path=${WORKDIR}/com/gens/dev/gefs.${PDY}/${cyc}
+
+hsi mkdir -p ${destination_path}
+
+cd ${output_path}
+htar -cvf ${destination_path}/gefs.${PDY}_${cyc}.pgrb2ap25.tar pgrb2ap25
+
+
+
