36240e7d2430d04916d1a9b11e34d8be6dd19693
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2019-11-25 14:33:44 -0500

    Add FV3GFS fix folder: fix_am and fix_fv3_gmted2010/C384     for the coming building new vertical structure  On branch XX_Master_NewBuild  Changes to be committed: 	modified:   ../fix/.gitignore 	modified:   ../sorc/link_gefs.sh
    Refs: #64583, #71500 and #56936

--- sorc/link_gefs.sh
+++ sorc/link_gefs.sh
@@ -31,22 +31,39 @@ pwd=$(pwd -P)
 #------------------------------
 if [ $machine == "cray" ]; then
     FIX_DIR="/gpfs/hps3/emc/ensemble/noscrub/emc.enspara/common/git/fv3gefs/fix_sst721_short"
+    FIX_DIR_FV3="/gpfs/hps3/emc/global/noscrub/emc.glopara/git/fv3gfs/fix"
 elif [ $machine = "dell" ]; then
     FIX_DIR="/gpfs/dell2/emc/verification/noscrub/emc.enspara/common/git/fv3gefs/fix_sst721_short"
+    FIX_DIR_FV3="/gpfs/dell2/emc/modeling/noscrub/emc.glopara/git/fv3gfs/fix"
 elif [ $machine = "theia" ]; then
     FIX_DIR="/scratch4/NCEPDEV/ensemble/noscrub/common/git/fv3gefs/fix_sst721"
+    FIX_DIR_FV3="/scratch4/NCEPDEV/global/save/glopara/git/fv3gfs/fix"
 elif [ $machine = "hera" ]; then
     FIX_DIR="/scratch2/NCEPDEV/ensemble/noscrub/common/git/fv3gefs/fix_sst721_short"
+    FIX_DIR_FV3="/scratch1/NCEPDEV/global/glopara/fix"
 fi
 
 # Delete Fix folder and relink/recopy it
-cd ${pwd}/../
-if [[ -d fix/fix_gefs ]]; then
+cd ${pwd}/../fix
+if [[ -d fix_gefs ]]; then
     echo "Fix folder exists, deleting it..."
-    rm -f fix/fix_gefs
+    rm -f fix_gefs
 fi
 
-$LINK $FIX_DIR fix/fix_gefs
+$LINK $FIX_DIR fix_gefs
+
+# fix_am
+sFolder=fix_am
+[[ -d $sFolder ]] && rm -rf $sFolder
+$LINK $FIX_DIR_FV3/$sFolder $sFolder
+
+# fix_fv3_gmted2010/C384
+sFolder=fix_fv3_gmted2010/C384
+[[ -d $sFolder ]] && rm -rf $sFolder
+sFolder2=fix_fv3_gmted2010
+[[ -z $sFolder2 ]] && mkdir $sFolder2
+$LINK $FIX_DIR_FV3/$sFolder $sFolder
+
 
 exit 0
 
