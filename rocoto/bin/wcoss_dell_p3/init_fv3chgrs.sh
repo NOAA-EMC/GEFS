eafa8a5a6aa73fb571eee8d842efe01778e2667a
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2020-02-07 13:26:38 -0500

    1) Modify bin file for init_chgrs to add loading modules 2) Update the versions/gefs_wcoss_dell_p3.ver  On branch XX_Master_NewBin 	modified:   bin/wcoss_dell_p3/init_fv3chgrs.sh 	modified:   ../versions/gefs_wcoss_dell_p3.ver
    Refs: #64583, #71500 and #56936

--- rocoto/bin/wcoss_dell_p3/init_fv3chgrs.sh
+++ rocoto/bin/wcoss_dell_p3/init_fv3chgrs.sh
@@ -6,8 +6,33 @@ set -x
 ulimit -s unlimited
 ulimit -a
 
-#export OMP_NUM_THREADS_CH=${GEFS_TPP:-28}
 
-#export FORECAST_SEGMENT=hr
+
+# module_ver.h
+. $SOURCEDIR/versions/gefs_wcoss_dell_p3.ver
+
+# Load modules
+#. /usrx/local/prod/lmod/lmod/init/ksh
+. /usrx/local/prod/lmod/lmod/init/profile
+module list
+
+module load EnvVars/$EnvVars_ver
+module load ips/$ips_ver
+module load impi/$impi_ver
+#module load grib_util/$grib_util_ver
+module load prod_util/$prod_util_ver
+module load prod_envir/$prod_envir_ver
+module load NetCDF/$NetCDF_ver
+
+module load lsf/$lsf_ver
+
+module list
+
+# For Development
+. $GEFS_ROCOTO/bin/wcoss_dell_p3/common.sh
+
+# Export List
+export OMP_NUM_THREADS_CH=${GEFS_TPP:-28}
+
 
 $SOURCEDIR/jobs/JGEFS_INIT_FV3CHGRS
