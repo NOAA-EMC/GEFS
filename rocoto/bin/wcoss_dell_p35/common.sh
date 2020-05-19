3687d205d810140130cad1f62d7e6ffef96fb625
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2020-05-19 16:41:59 -0400

    1) Add restart_interval_aer to user configre files with default 12 2) Change default value of restart_interval_aer is 12 3) Unify all SLEEP_TIME and SLEEP_INT values
     On branch XX_Master_EE2_ECF
     Changes to be committed:
    	modified:   ../parm/gefs.parm
    	modified:   ../parm/gefs_atmos_ensavg_nemsio.parm
    	modified:   ../parm/gefs_ensstat.parm
    	modified:   ../parm/gefs_gempak.parm
    	modified:   ../parm/gefs_post.parm
    	modified:   ../parm/gefs_prdgen.parm
    	modified:   bin/wcoss_dell_p35/common.sh
    	modified:   py/user_cray.conf
    	modified:   py/user_hera.conf
    	modified:   py/user_wcoss_dell_p3.conf
    	modified:   py/user_wcoss_dell_p35.conf
    	modified:   user_full.conf
    
    Refs: #76266, #64583, #62644, #71500 and #56936

--- rocoto/bin/wcoss_dell_p35/common.sh
+++ rocoto/bin/wcoss_dell_p35/common.sh
@@ -61,7 +61,8 @@ elif [[ $RocotoGen == 1 ]]; then
     export npert=2
     export navg_min=1 
     #export fhmax="840"
-    #export fhrestart="48"
+    #export fhrestart=24
+    #export restart_interval_aer=12
     ### For keep and arhive data
     #export KEEP_DIR="/gpfs/dell6/emc/modeling/noscrub/emc.enspara/Xianwu.Xue/GEFS/GEFSv12_Test02"
     #export HPSS_DIR="/NCEPDEV/emc-ensemble/2year/emc.enspara/Xianwu.Xue/GEFS_DELL35/GEFSv12_Test02"
