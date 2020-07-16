e50e0f2f1c93c4a0bb9026d79d25e93c983b94e2
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2020-07-15 21:57:53 -0400

    Modify ush/gefs_atmos_prep.sh to decrease the runtime
     On branch feature/recenter
    	modified:   ush/gefs_atmos_prep.sh
    
     Refs: #76266

--- ush/gefs_atmos_prep.sh
+++ ush/gefs_atmos_prep.sh
@@ -24,7 +24,11 @@ cd $INIDIR
 # Surface file must always be from current GFS cycle
 SFCFILE=$COMINgfs/gfs.t${cyc}z.sfcanl.nemsio
 if [[ -f $SFCFILE ]]; then
+if [[ $mem = c00 ]] ;then
+    echo "sfc chgres will be carried out in p01"
+else
     $NCP $SFCFILE $INIDIR/sfnanl.gfs.$PDY$cyc
+fi
 else
     msg="FATAL ERROR in ${.sh.file}: GFS surfce analysis $SFCFILE not found!"
     echo $msg
@@ -74,23 +78,175 @@ else
 fi
 
 #############################################################
+export OMP_NUM_THREADS_CH=${OMP_NUM_THREADS_CH:-24}
+export APRUNC=${APRUNC:-"time"}
+export CASE=${CASE:-C96}                     # resolution of tile: 48, 96, 192, 384, 768, 1152, 3072
+export CRES=`echo $CASE | cut -c 2-`
+export CDATE=${CDATE:-${cdate:-2017031900}}  # format yyyymmddhh yyyymmddhh ...
+export CDUMP=${CDUMP:-gfs}                   # gfs or gdas
+export LEVS=${LEVS:-65}
+export LSOIL=${LSOIL:-4}
+export REGIONAL=${REGIONAL:-0}               # default is to assume uniform grid, which is REGIONAL=0
+export VERBOSE=YES
+pwd=$(pwd)
+export NWPROD=${NWPROD:-$pwd}
+export HOMEgfs=${HOMEgfs:-$NWPROD/gfs.v15.0.0}
+export FIXfv3=${FIXfv3:-$HOMEgfs/fix/fix_fv3_gmted2010}
+export FIXam=${FIXam:-$HOMEgfs/fix/fix_am}
+export CHGRESEXEC=$HOMEgfs/exec/global_chgres
+export CHGRESSH=$HOMEgfs/ush/global_chgres.sh
+
+# Location of initial conditions for GFS (before chgres) and FV3 (after chgres)
+export INIDIR=${INIDIR:-$pwd}
+export OUTDIR=${OUTDIR:-$pwd/INPUT}
+mkdir -p $OUTDIR
+
+export gtype=${gtype:-uniform}      # grid type = uniform, stretch, nest or stand alone regional
+export ntiles=6
+
+#---------------------------------------------------------------
+
+# Temporary rundirectory
+export DATA=${DATA:-${RUNDIR:-$pwd/rundir$$}}
+if [ ! -s $DATA ]; then mkdir -p $DATA; fi
+cd $DATA || exit 8
+
+export ymd=`echo $CDATE | cut -c 1-8`
+export cyc=`echo $CDATE | cut -c 9-10`
+
+export ictype='fv3gfs'
+
+   export ATMANL=$INIDIR/gfnanl.${CDUMP}.$CDATE
+   export SFCANL=$INIDIR/sfnanl.${CDUMP}.$CDATE
+  if [ -s ${INIDIR}/sfnanl.${CDUMP}.$CDATE ]; then
+  export SFCANL=$INIDIR/sfnanl.${CDUMP}.$CDATE
+  else
+  export SFCANL=NULL
+  fi
+   export NSTANL=NULL
+ export SOILTYPE_INP=statsgo
+ export VEGTYPE_INP=igbp
+ export nopdpvv=.false.
+ export NTRAC=7
+
+ LONB_ATM=0   # not used for
+ LATB_ATM=0   # ops files
+ JCAP_CASE=$((CRES*2-2))
+ LONB_SFC=$((CRES*4))
+ LATB_SFC=$((CRES*2))
+
+# to use new albedo, soil/veg type
+export CLIMO_FIELDS_OPT=3
+export LANDICE_OPT=${LANDICE_OPT:-2}
+export IALB=1
+export SOILTYPE_OUT=statsgo
+export VEGTYPE_OUT=igbp
+export FNZORC=igbp
+
+export SIGLEVEL=${FIXam}/global_hyblev.l${LEVS}.txt
+export FNGLAC=${FIXam}/global_glacier.2x2.grb
+export FNMXIC=${FIXam}/global_maxice.2x2.grb
+export FNTSFC=${FIXam}/cfs_oi2sst1x1monclim19822001.grb
+export FNSNOC=${FIXam}/global_snoclim.1.875.grb
+export FNALBC2=${FIXam}/global_albedo4.1x1.grb
+export FNAISC=${FIXam}/cfs_ice1x1monclim19822001.grb
+export FNTG3C=${FIXam}/global_tg3clim.2.6x1.5.grb
+export FNVEGC=${FIXam}/global_vegfrac.0.144.decpercent.grb
+export FNVMNC=${FIXam}/global_shdmin.0.144x0.144.grb
+export FNVMXC=${FIXam}/global_shdmax.0.144x0.144.grb
+export FNSLPC=${FIXam}/global_slope.1x1.grb
+export FNMSKH=${FIXam}/global_slmask.t1534.3072.1536.grb
+export FNSMCC=$FIXam/global_soilmgldas.statsgo.t${JCAP_CASE}.${LONB_SFC}.${LATB_SFC}.grb
+export FNSOTC=$FIXam/global_soiltype.statsgo.t${JCAP_CASE}.${LONB_SFC}.${LATB_SFC}.rg.grb
+export FNVETC=$FIXam/global_vegtype.igbp.t${JCAP_CASE}.${LONB_SFC}.${LATB_SFC}.rg.grb
+export FNABSC=$FIXam/global_mxsnoalb.uariz.t${JCAP_CASE}.${LONB_SFC}.${LATB_SFC}.rg.grb
+export FNALBC=$FIXam/global_snowfree_albedo.bosu.t${JCAP_CASE}.${LONB_SFC}.${LATB_SFC}.rg.grb
+#------------------------------------------------
+# Convert atmospheric file.
+#------------------------------------------------
+
+  export CHGRESVARS="use_ufo=.false.,idvc=2,nvcoord=2,idvt=21,idsl=1,IDVM=0,nopdpvv=$nopdpvv"
+  export SIGINP=$ATMANL
+  export SFCINP=NULL
+  export NSTINP=NULL
+  export JCAP=$JCAP_CASE
+  export LATB=$LATB_ATM
+  export LONB=$LONB_ATM
+  $CHGRESSH
+  rc=$?
+  if [[ $rc -ne 0 ]] ; then
+   echo "***ERROR*** rc= $rc"
+   exit $rc
+  fi
+
+  mv ${DATA}/gfs_data.tile*.nc  $OUTDIR/.
+  mv ${DATA}/gfs_ctrl.nc        $OUTDIR/.
+
+#---------------------------------------------------
+# Convert surface and nst files one tile at a time.
+#---------------------------------------------------
+
+  export CHGRESVARS="use_ufo=.true.,idvc=2,nvcoord=2,idvt=21,idsl=1,IDVM=0,nopdpvv=$nopdpvv"
+  export SIGINP=NULL
+  export SFCINP=$SFCANL
+  export NSTINP=$NSTANL
+  export JCAP=$JCAP_CASE
+  export LATB=$LATB_SFC
+  export LONB=$LONB_SFC
+  if [[ $SFCANL = "NULL" ]]; then
+  echo "sfc chgres skipped"
+  else
+    tile=1
+    while [ $tile -le $ntiles ]; do
+      export TILE_NUM=$tile
+      $CHGRESSH
+      rc=$?
+      if [[ $rc -ne 0 ]] ; then
+        echo "***ERROR*** rc= $rc"
+        exit $rc
+      fi
+      mv ${DATA}/out.sfc.tile${tile}.nc $OUTDIR/sfc_data.tile${tile}.nc
+      tile=`expr $tile + 1 `
+    done
+  fi # $SFCINP = NULL
+
 # Execute the script
-$USHgfs/global_chgres_driver.sh
-export err=$?
-if [[ $err != 0 ]]; then
-    echo "FATAL ERROR in ${.sh.file}: global_chgres_driver failed!"
-    exit $err
-fi
+#$USHgfs/global_chgres_driver.sh
+#export err=$?
+#if [[ $err != 0 ]]; then
+#    echo "FATAL ERROR in ${.sh.file}: global_chgres_driver failed!"
+#    exit $err
+#fi
 #############################################################
 
 mkdir -p $GESOUT/init/$mem
-$NCP $OUTDIR/sfc* $GESOUT/init/$mem
-$NCP $OUTDIR/gfs_ctrl.nc $GESOUT/init/$mem
+
+if [[ $mem == "c00" ]]; then
+    $NCP $OUTDIR/gfs_ctrl.nc $GESOUT/init/$mem
+elif [[ $mem == "p01" ]]; then
+    mkdir -p $GESOUT/init/c00
+    $NCP $OUTDIR/sfc* $GESOUT/init/c00
+    $NCP $OUTDIR/sfc* $GESOUT/init/$mem
+    $NCP $OUTDIR/gfs_ctrl.nc $GESOUT/init/$mem
+else
+    $NCP $OUTDIR/sfc* $GESOUT/init/$mem
+    $NCP $OUTDIR/gfs_ctrl.nc $GESOUT/init/$mem
+fi
 
 if [[ $SENDCOM == "YES" ]]; then
     mkdir -p $COMOUT/init/$mem
-    $NCP $OUTDIR/sfc* $COMOUT/init/$mem
-    $NCP $OUTDIR/gfs_ctrl.nc $COMOUT/init/$mem
+    if [[ $mem == "c00" ]]; then
+        $NCP $OUTDIR/gfs_ctrl.nc $COMOUT/init/$mem
+    elif [[ $mem == "p01" ]]; then
+        mkdir -p $COMOUT/init/c00
+        $NCP $OUTDIR/sfc* $COMOUT/init/c00
+        $NCP $OUTDIR/sfc* $COMOUT/init/$mem
+        $NCP $OUTDIR/gfs_ctrl.nc $COMOUT/init/$mem
+    else
+        $NCP $OUTDIR/sfc* $COMOUT/init/$mem
+        $NCP $OUTDIR/gfs_ctrl.nc $COMOUT/init/$mem
+    fi
+
     if [[ $SENDDBN = YES ]];then
       $DBNROOT/bin/dbn_alert MODEL ENS_CTR_$mem $job $COMOUT/init/$mem/gfs_ctrl.nc
       $DBNROOT/bin/dbn_alert MODEL ENS_MSC_$mem $job $COMOUT/init/$mem/sfc_data.tile6.nc
@@ -104,5 +260,6 @@ if [[ $SENDCOM == "YES" ]]; then
 fi
 
 echo "$(date -u) end ${.sh.file}"
+exit $rc
 
-exit $err
+
