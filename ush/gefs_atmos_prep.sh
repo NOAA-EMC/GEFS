bf0c00fbc43a8c5b3f9420f6de685fe22169ccaf
Bing Fu <bing.fu@noaa.gov>
2020-07-20 13:41:14 +0000

    modification of atm_prep script to process gfsv16 netcdf enkf f06 and analysis
    Change-Id: Ia514c4adeab85e1ced62e80f2ada2adc2644e31d

--- ush/gefs_atmos_prep.sh
+++ ush/gefs_atmos_prep.sh
@@ -22,9 +22,9 @@ mkdir -p $OUTDIR
 cd $INIDIR
 
 # Surface file must always be from current GFS cycle
-SFCFILE=$COMINgfs/gfs.t${cyc}z.sfcanl.nemsio
+SFCFILE=$COMINgfs/gfs.t${cyc}z.sfcanl.nc
 if [[ -f $SFCFILE ]]; then
-    $NCP $SFCFILE $INIDIR/sfnanl.gfs.$PDY$cyc
+    $NCP $SFCFILE $INIDIR
 else
     msg="FATAL ERROR in ${.sh.file}: GFS surfce analysis $SFCFILE not found!"
     echo $msg
@@ -34,33 +34,37 @@ fi
 
 if [[ $mem = c00 ]] ;then
     # Control intial conditions must be from current GFS cycle
-    ATMFILE=$COMINgfs/gfs.t${cyc}z.atmanl.nemsio
+    ATMFILE=$COMINgfs/gfs.t${cyc}z.atmanl.nc
     if [[ -f $ATMFILE ]]; then
-        $NCP $ATMFILE $INIDIR/gfnanl.gfs.$PDY$cyc
+        $NCP $ATMFILE $INIDIR
+        export ATM_FILES_INPUT="gfs.t${cyc}z.atmanl.nc"
+        export SFC_FILES_INPUT="gfs.t${cyc}z.sfcanl.nc"
     else
         msg="FATAL ERROR in ${.sh.file}: GFS atmospheric analysis file $ATMFILE not found!"
         echo "$msg"
         export err=101
         err_chk || exit $err
     fi
+export CONVERT_SFC=".true."
 else
     i=0
     success="NO"
 	(( cmem = nmem + memshift ))
-    while [[ $success == "NO" && $i < $MAX_ENKF_SEARCHES ]]; do
+    while [[ $success == "NO" && i < MAX_ENKF_SEARCHES ]]; do
         if (( cmem > 80 )); then
             (( cmem = cmem - 80 ))
         fi
 
         memchar="mem"$(printf %03i $cmem)
-        ATMFILE="$COMINenkf$pdyp/$cycp/$memchar/gdas.t${cycp}z.atmf006.nemsio"
+        ATMFILE="$COMINenkf$pdyp/$cycp/$memchar/gdas.t${cycp}z.atmf006.nc"
     
         if [[ -f $ATMFILE ]]; then
-            $NCP $ATMFILE $INIDIR/gfnanl.gfs.$PDY$cyc
+            $NCP $ATMFILE $INIDIR
+        export ATM_FILES_INPUT="gdas.t${cycp}z.atmf006.nc"
             success="YES"
         else
-            (( i = i + 1 ))
-            if [[ $i < $MAX_ENKF_SEARCHES ]]; then
+            i=i+1
+            if [[ i < MAX_ENKF_SEARCHES ]]; then
                 echo "EnKF atmospheric file $ATMFILE not found, trying different member"
                 (( cmem = cmem + ENKF_SEARCH_LEAP ))
             else
@@ -70,36 +74,61 @@ else
                 err_chk || exit $err
             fi
         fi # [[ -f $ATMFILE ]]
-    done # [[ $success == "NO" && $i < $MAX_ENKF_SEARCHES ]]
+    done # [[ success == "NO" && i < MAX_ENKF_SEARCHES ]]
+export CONVERT_SFC=".false."
 fi
-
+export CRES=$(echo $CASEHR |cut -c2-5)
+export HOMEufs=$HOMEgfs/sorc/global-workflow.fd/sorc/ufs_utils.fd
+export COMIN=$INIDIR
+export INPUT_TYPE="gaussian_netcdf"
+export FIXfv3=$FIXgfs/fix_fv3_gmted2010/C$CRES
+export FIXsfc=$FIXfv3/fix_sfc
 #############################################################
 # Execute the script
-$USHgfs/global_chgres_driver.sh
+$USHgfs/chgres_cube.sh
 export err=$?
 if [[ $err != 0 ]]; then
     echo "FATAL ERROR in ${.sh.file}: global_chgres_driver failed!"
     exit $err
 fi
 #############################################################
-
 mkdir -p $GESOUT/init/$mem
-$NCP $OUTDIR/sfc* $GESOUT/init/$mem
+#$NCP $OUTDIR/sfc* $GESOUT/init/$mem
 $NCP $OUTDIR/gfs_ctrl.nc $GESOUT/init/$mem
+        if [[ $mem = c00 ]] ;then
+  mv ${DATA}/gfs_ctrl.nc       $OUTDIR/.
+
+  mv ${DATA}/out.atm.tile1.nc  $OUTDIR/gfs_data.tile1.nc
+  mv ${DATA}/out.atm.tile2.nc  $OUTDIR/gfs_data.tile2.nc
+  mv ${DATA}/out.atm.tile3.nc  $OUTDIR/gfs_data.tile3.nc
+  mv ${DATA}/out.atm.tile4.nc  $OUTDIR/gfs_data.tile4.nc
+  mv ${DATA}/out.atm.tile5.nc  $OUTDIR/gfs_data.tile5.nc
+  mv ${DATA}/out.atm.tile6.nc  $OUTDIR/gfs_data.tile6.nc
+
+  mv ${DATA}/out.sfc.tile1.nc $OUTDIR/sfc_data.tile1.nc
+  mv ${DATA}/out.sfc.tile2.nc $OUTDIR/sfc_data.tile2.nc
+  mv ${DATA}/out.sfc.tile3.nc $OUTDIR/sfc_data.tile3.nc
+  mv ${DATA}/out.sfc.tile4.nc $OUTDIR/sfc_data.tile4.nc
+  mv ${DATA}/out.sfc.tile5.nc $OUTDIR/sfc_data.tile5.nc
+  mv ${DATA}/out.sfc.tile6.nc $OUTDIR/sfc_data.tile6.nc
+
+        else
+
+  mv ${DATA}/gfs_ctrl.nc       $OUTDIR/.
+  mv ${DATA}/out.atm.tile1.nc  $OUTDIR/gfs_data.tile1.nc
+  mv ${DATA}/out.atm.tile2.nc  $OUTDIR/gfs_data.tile2.nc
+  mv ${DATA}/out.atm.tile3.nc  $OUTDIR/gfs_data.tile3.nc
+  mv ${DATA}/out.atm.tile4.nc  $OUTDIR/gfs_data.tile4.nc
+  mv ${DATA}/out.atm.tile5.nc  $OUTDIR/gfs_data.tile5.nc
+  mv ${DATA}/out.atm.tile6.nc  $OUTDIR/gfs_data.tile6.nc
+        fi
 
 if [[ $SENDCOM == "YES" ]]; then
     mkdir -p $COMOUT/init/$mem
     $NCP $OUTDIR/sfc* $COMOUT/init/$mem
     $NCP $OUTDIR/gfs_ctrl.nc $COMOUT/init/$mem
-    if [[ $SENDDBN = YES ]];then
-      $DBNROOT/bin/dbn_alert MODEL ENS_CTR_$mem $job $COMOUT/init/$mem/gfs_ctrl.nc
-      $DBNROOT/bin/dbn_alert MODEL ENS_MSC_$mem $job $COMOUT/init/$mem/sfc_data.tile6.nc
-    fi
     if [[ $mem == "c00" ]]; then
-      $NCP $OUTDIR/gfs_data*.nc $COMOUT/init/$mem
-      if [[ $SENDDBN = YES ]];then
-              $DBNROOT/bin/dbn_alert MODEL ENS_SA_$mem $job $COMOUT/init/$mem/gfs_data.tile6.nc
-      fi
+        $NCP $OUTDIR/gfs_data*.nc $COMOUT/init/$mem
     fi
 fi
 
