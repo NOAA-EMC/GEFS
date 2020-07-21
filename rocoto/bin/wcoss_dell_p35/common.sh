b33485e96f0668d8ea9ea9c9ceeaa7a4f2fbb3ea
Bing Fu <bing.fu@noaa.gov>
2020-07-21 14:25:11 +0000

    update module files, add APRUN for chgres etc.
    Change-Id: I9356b2328d0638de54e977c7fc246486d49939f6

--- rocoto/bin/wcoss_dell_p35/common.sh
+++ rocoto/bin/wcoss_dell_p35/common.sh
@@ -9,7 +9,7 @@ export RUN_ENVIR=${RUN_ENVIR:-dev}
 export total_tasks=${GEFS_NTASKS}
 export OMP_NUM_THREADS=${GEFS_TPP}
 export taskspernode=${GEFS_PPN}
-
+export total_tasks=31
 # Calculate the number of tasks based on the task geometry
 #(( NTASKS=$(echo $LSB_PJL_TASK_GEOMETRY | grep -Po "\d+" | sort -n | tail -1) + 1 ))
 #export NTASKS
@@ -24,7 +24,7 @@ if [[ $RocotoGen == 0 ]]; then
     export APRUN_CHGRES="mpirun -n 1"
     #export aprun_gec00="mpirun -n 1"
     export APRUN_CALCINC="mpirun -n 1"
-
+    export APRUN="mpirun -n 6"
     . $GEFS_ROCOTO/parm/setbase
     . $GEFS_ROCOTO/parm/gefs_config
     . $GEFS_ROCOTO/parm/gefs_dev.parm
