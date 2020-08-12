b514dc190a40bfe0bdb5ee8623d9679abe1c22bd
Dingchen Hou <Dingchen.Hou@noaa.gov>
2020-08-12 14:23:11 -0400

    Reverse unnecessary chages for easy commit in the future
    No real impact to users
    
    Refs: #81079

--- scripts/exgefs_atmos_prep.sh
+++ scripts/exgefs_atmos_prep.sh
@@ -15,9 +15,8 @@ export FIXfv3=${FIXfv3:-$FIXgfs/fix_fv3_gmted2010}
 
 cd $DATA
 rm -rf mpmd_cmdfile*
+
 for mem in $memberlist; do
-	mkdir -p $GESOUT/init/$mem
-	mkdir -p $COMOUT/init/$mem
 	echo "$USHgefs/gefs_atmos_prep.sh $mem" >> mpmd_cmdfile
 done
 
@@ -35,8 +34,8 @@ export MP_PGMMODEL=mpmd
 $APRUN_MPMD
 export err=$?
 if [[ $err != 0 ]]; then
-	echo "FATAL ERROR in ${.sh.file}: One or more atmos_prep jobs in $MP_CMDFILE failed!"
-	exit $err
+    echo "FATAL ERROR in ${.sh.file}: One or more atmos_prep jobs in $MP_CMDFILE failed!"
+    exit $err
 fi
 #############################################################
 
