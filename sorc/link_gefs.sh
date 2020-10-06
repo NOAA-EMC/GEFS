7798264755bb739c6871da8568ffb234e9c5589b
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2020-10-06 13:31:19 -0400

    Fix bugs in chem_init & add new files to link
    1) Fix bugs in bin files of chem_init task on p3 & p35
        to use new NetCDF[HDF5]-parallel libs
    2) Improve sorc/link_gefs.sh to delete unused EXE and
        add new needed EXE
    
     On branch release/gefs_v12
     Changes to be committed:
    	modified:   bin/wcoss_dell_p3/chem_init.sh
    	modified:   bin/wcoss_dell_p35/chem_init.sh
    	modified:   ../sorc/link_gefs.sh
    
    Refs: #81079

--- sorc/link_gefs.sh
+++ sorc/link_gefs.sh
@@ -192,12 +192,13 @@ if [[ -d global-workflow.fd ]]; then
     # for init_aerosol
     $LINK ../sorc/global-workflow.fd/ush/merge_fv3_chem_tile.py ../ush/
     $LINK ../sorc/global-workflow.fd/exec/chgres_recenter.exe ../exec/
-    $LINK ../sorc/global-workflow.fd/sorc/gsi.fd/exec/calc_increment_ens_gsdchem.x ../exec/
 fi
 
 # for atmos_prep for GFSv16
 if [[ -d global-workflow.fd ]]; then
     $LINK ../sorc/global-workflow.fd/sorc/ufs_utils.fd/exec/chgres_cube ../exec/
+    $LINK ../sorc/global-workflow.fd/exec/chgres_recenter_ncio.exe ../exec/
+    $LINK ../sorc/global-workflow.fd/sorc/gsi.fd/exec/calc_increment_ens_ncio.x ../exec/
 fi
 
 
