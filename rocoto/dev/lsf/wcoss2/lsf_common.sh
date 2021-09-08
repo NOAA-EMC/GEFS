efbce2493108f40eaecc719f3ca014b902a8db72
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-09-08 18:00:58 -0400

    Improve to use compath.py to get COM[IN,OUT,INgfs ]
    1) Improve j-job to use compath.py to get all COM paths
    2) Improve j-job to load gefs.parm then set COMINgfs[...]
    	using compath.py to set ver_gfs[...] values
    3) Improve jobcards (ecf) to consistent with these changes
    
    Please note that I add "export envir=prod" before calling
    j-job to make compath.py can find the right path for COMIN
    COMOUT, GESIN, GESOUT
    
     On branch feature/ops_port2wcoss2
    
    Refs: #23

--- rocoto/dev/lsf/wcoss2/lsf_common.sh
+++ rocoto/dev/lsf/wcoss2/lsf_common.sh
@@ -13,25 +13,22 @@ export RUN_ENVIR=${RUN_ENVIR:-dev}
 export NET=${NET:-gefs}
 export RUN=${RUN:-gefs}
 
-export ver_gefs=v12.2
-export ver_gfs=v16.2
-export ver_cfs=v2.3
-
-export HOMEgefs=${HOMEgefs:-/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/gefs_port2wcoss2}
-export WORKDIR=/lfs/h2/emc/ptmp/Xianwu.Xue/o/gefs_port2wcoss2
+export EXPID=port2wcoss2_new
+export HOMEgefs=${HOMEgefs:-/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/$EXPID}
+export WORKDIR=/lfs/h2/emc/ptmp/Xianwu.Xue/o/$EXPID
 
 export GEFS_ROCOTO=${HOMEgefs}/rocoto
 
 #export HOMEdata=/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/HOMEdata
 export HOMEdata=/lfs/h1/ops
 
-export COMPATH=$HOMEdata/canned/com/gfs:$HOMEdata/canned/com/cfs
+export COMPATH=$HOMEdata/canned/com/gfs:$HOMEdata/canned/com/cfs:${WORKDIR}/$envir/com/${NET}
 export DCOMROOT=${HOMEdata}/canned/dcom
 
 #===
 export job=${job:-$PBS_JOBNAME}
 
-export COMROOT=${WORKDIR}/com
+export COMROOT=${WORKDIR}/$envir/com
 export GESROOT=${WORKDIR}/nwges
 export DATAROOT=${WORKDIR}/tmp
 
@@ -41,51 +38,24 @@ export SENDECF=NO       # ecflow YES
 export SENDDBN=NO       # ecflow YES
 export SENDDBN_NTC=NO   # ecflow YES
 
+test=${test:-"test2"}
+if [[ $test == "test1" ]]; then
+	export layout_x=6
+	export layout_y=8
+	export WRITE_GROUP=1
+	export WRTTASK_PER_GROUP=32
+	export npe_wav=${npe_wav:-160}
 
-# ---------- Keep for future reference ---------
-##. $GEFS_ROCOTO/bin/wcoss2/common.sh
-##====
-#UseData="keep_for_future_ref" #old, canned, new
-#if [[ UseData == "old" ]]; then
-#	HOMEdata=/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/HOMEdata/com
-#	export COMINgfs_base=${HOMEdata}/gfs/prod/gfs.
-#	export COMINenkf_base=${HOMEdata}/gfs/prod/enkfgdas.
-#	export COMINcfs_base=${HOMEdata}/cfs/prod/cfs/cfs.
-#	export COMINgfs=${COMINgfs:-${COMINgfs_base}${PDY}/$cyc/atmos}
-#	export COMINenkf=${COMINenkf:-${COMINenkf_base}${pdyp}/$cycp/atmos}
-#
-#	export COMIN_WAV_ICE=$COMINgfs
-#	#export COMINenkf=${COMINenkf:-${COMINenkf_base}}
-#	export COMINcfs=${COMINcfs:-${COMINcfs_base}}
-#
-#	export DCOMROOT=/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/HOMEdata/dcom/prod
-#elif [[ UseData == "canned" ]]; then
-#	HOMEdata=/lfs/h2/emc/ens/noscrub/Xianwu.Xue/gefs/HOMEdata
-#	export COMPATH=$HOMEdata/canned/com/gfs:$HOMEdata/canned/com/cfs
-##	compath.py canned/com/gfs/${ver_gfs}
-##	compath.py canned/com/gfs/${ver_cfs}
-#
-#	export COMINgfs=$(compath.py canned/com/gfs/${ver_gfs})/gfs.${PDY}/${cyc}/atmos
-#	export COMIN_WAV_ICE=$COMINgfs
-#	export COMINenkf=$(compath.py compath.py canned/com/gfs/${ver_gfs})/enkfgdas.${pdyp}/${cycp}/atmos
-#
-#	export DCOMROOT=${HOMEdata}/canned/dcom
-#	# /lfs/h1/ops/canned/com/
-#	# export DCOMROOT=/lfs/h1/ops/canned/dcom
-#	# export COMPATH=/lfs/h1/ops/canned/com/blend:/lfs/h1/ops/canned/com/gfs:/lfs/h1/ops/canned/com/gefs
-##	HOMEdata=/lfs/h1/emc/ens/noscrub/Xianwu.Xue/gefs/HOMEdata/com
-##    export COMINgfs_base=${HOMEdata}/gfs/prod/gfs. #/gpfs/dell3/ptmp/emc.glopara/ROTDIRS/v16rt2/gfs/para/gfs.
-##    #export COMINgfs_base=$(compath.py gfs/prod)/gfs.
-##    export COMINenkf_base=${HOMEdata}/gfs/prod/enkfgdas.
-##    #export COMINenkf_base=$(compath.py gfs/prod)/enkfgdas.
-##    export COMINcfs_base=${HOMEdata}/cfs/prod/cfs/cfs.
-##    #export COMINcfs_base=$(compath.py cfs/prod)/cfs/cfs.
-##    export COMIN_WAV_ICE=${HOMEdata}/gfs/prod/gfs.${PDY}/${cyc}/atmos
-##    #export COMIN_WAV_ICE=$(compath.py gfs/prod)/gfs.${PDY}/${cyc}/atmos
-##    export COMINgfs=${COMINgfs:-${COMINgfs_base}${PDY}/$cyc/atmos}
-##    export COMINenkf=${COMINenkf:-${COMINenkf_base}${pdyp}/$cycp/atmos}
-#fi
-#
-##echo "DCOMROOT=$DCOMROOT"
-##echo "COMINgfs=$COMINgfs"
+	export layout_x_chem=6
+	export layout_y_chem=8
+	export WRITE_GROUP_chem=${WRITE_GROUP_chem:-1}
+	export WRTTASK_PER_GROUP_chem=${WRTTASK_PER_GROUP_chem:-42}
+
+	export layout_x_lr=${layout_x_lr:-8}
+	export layout_y_lr=${layout_y_lr:-8}
+	export WRITE_GROUP_lr=${WRITE_GROUP_lr:-1}
+	export WRTTASK_PER_GROUP_lr=${WRTTASK_PER_GROUP_lr:-32} #or 42, 48
+
+fi
+echo $layout_x
 
