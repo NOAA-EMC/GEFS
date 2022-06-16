2d8e05b1e4d76a3d9f5ed0cda352af456f1be7d3
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2022-06-16 13:12:58 -0400

    Update the GEFS FIX and g-w check branch
     On branch feature/gefs_v12_3
     Changes to be committed:
    	modified:   sorc/checkout.sh
    	modified:   sorc/link_gefs.sh
    
    Refs: #56

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -15,7 +15,7 @@ if [[ ! -d global-workflow.fd ]] ; then
     rm -f ${logs_dir}/checkout-global-workflow.log
     git clone https://github.com/NOAA-EMC/global-workflow.git global-workflow.fd >>  ${logs_dir}/checkout-global-workflow.log 2>&1
     cd global-workflow.fd
-    git checkout gefs_v12.2.0-1
+    git checkout release/gefs_v12 #gefs_v12.2.0-1
 	cd sorc
     ./checkout.sh
     ERR=$?
