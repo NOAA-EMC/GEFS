3de25db50ba3105c6b96c3d45d0670d294071c17
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2023-04-25 17:27:16 -0400

    Improve bin file of forecast
     On branch feature/v13_atmos_fcst
    	modified:   rocoto/bin/hera/forecast_hr.sh
    	modified:   rocoto/bin/wcoss2/forecast_hr.sh
    
    Refs: #94

--- rocoto/bin/hera/forecast_hr.sh
+++ rocoto/bin/hera/forecast_hr.sh
@@ -1,24 +1,50 @@
-#!/bin/ksh
-#
+#! /usr/bin/env bash
 
-# EXPORT list here
 set -x
-export IOBUF_PARAMS=
-
-
 ulimit -s unlimited
 ulimit -a
 
-export MP_SHARED_MEMORY=yes
-export MEMORY_AFFINITY=core:2
+# module_ver.h
+. ${GEFS_ROCOTO}/dev/versions/run_hera.ver
+
+# Load modules
+module reset
+
+module use -a /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
+
+module load hpc/${hpc_ver}
+module load hpc-intel/${intel_ver}
+module load hpc-impi/${impi_ver}
+module load grib_util/${grib_util_ver}
+module load prod_util/${prod_util_ver}
+module load netcdf/${netcdf_ver}
+
+module load prod_envir/${prod_envir_ver}
+
+module list
+
+# For Development
+. $G{EFS_ROCOTO}/bin/hera/common.sh
+
+# Export List
+export OMP_NUM_THREADS=1
+
+#export MP_SHARED_MEMORY=yes
+#export MEMORY_AFFINITY=core:2
 
 #export NTHREADS_SIGCHGRS=2
 
 #export FORECAST_SEGMENT=hr
 
-export memdir_template='$ROTDIR/enkf.$CDUMP.$PDY/$cyc'
+#export memdir_template='$ROTDIR/enkf.$CDUMP.$PDY/$cyc'
 
+shopt -s extglob
+ver=${gefs_ver%${gefs_ver##v+([0-9]).+([0-9])}}
+shopt -u extglob
+
+export ROTDIR=${COMROOT}/gefs/${ver}
+export ROTDIR_GFS=${HOMEdata}/gfs/${gfs_ver}
 
 # CALL executable job script here
-$SOURCEDIR/jobs/JGEFS_FORECAST
+${SOURCEDIR}/jobs/JGEFS_FORECAST
 
