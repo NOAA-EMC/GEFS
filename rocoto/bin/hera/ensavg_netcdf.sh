c1d23dc2784c57b76d403626b7006bf6735e1415
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2023-05-01 12:45:58 -0400

    Modify bin file of ensavg_netcdf for Hera
     On branch feature/v13_atmos_ensavg_netcdf
    	modified:   rocoto/bin/hera/ensavg_netcdf.sh
    
    Refs: #96

--- rocoto/bin/hera/ensavg_netcdf.sh
+++ rocoto/bin/hera/ensavg_netcdf.sh
@@ -1,16 +1,34 @@
-#!/bin/ksh
-#
+#! /usr/bin/env bash
 
-# EXPORT list here
 set -x
-export IOBUF_PARAMS=
-
-
 ulimit -s unlimited
 ulimit -a
 
-export MP_SHARED_MEMORY=no
-export MEMORY_AFFINITY=core:6
+# module_ver.h
+. ${GEFS_ROCOTO}/dev/versions/run_hera.ver
+
+# Load modules
+module reset
+
+module use -a /scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack
+
+module load hpc/${hpc_ver}
+module load hpc-intel/${intel_ver}
+module load hpc-impi/${impi_ver}
+module load grib_util/${grib_util_ver}
+module load prod_util/${prod_util_ver}
+module load netcdf/${netcdf_ver}
+
+module load prod_envir/${prod_envir_ver}
+
+module list
+
+# For Development
+. ${GEFS_ROCOTO}/bin/hera/common.sh
+
+# Export List
+#export MP_SHARED_MEMORY=no
+#export MEMORY_AFFINITY=core:6
 
 #export total_tasks=6
 #export OMP_NUM_THREADS=6
@@ -21,5 +39,5 @@ export FORECAST_SEGMENT=hr
 export NTHREADS_SIGCHGRS=6
 
 # CALL executable job script here
-$SOURCEDIR/jobs/JGEFS_ATMOS_ENSAVG_NETCDF
+${SOURCEDIR}/jobs/JGEFS_ATMOS_ENSAVG_NETCDF
 
